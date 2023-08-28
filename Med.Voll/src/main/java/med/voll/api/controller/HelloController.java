e7d85ea1d179b5c3474295fb767ade6b2df5531f
nascimento.reis <danilonascimento.dr@gmail.com>
2023-08-28 14:29:18 -0300

    TesteToken

--- Med.Voll/src/main/java/med/voll/api/controller/HelloController.java
+++ Med.Voll/src/main/java/med/voll/api/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping
+    @GetMapping //Mensagem de saida
     public String olaMundo() {
         return "Hello World Spring!";
     }

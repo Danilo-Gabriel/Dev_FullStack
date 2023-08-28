e94fe926fe86c91c9a3c0916cb0b2a084827cd0f
daniloGabriel <danilonascimento.dr@gmail.com>
2023-08-28 17:03:43 -0300

    Organizando Diretorios

--- studentJava/src/exListas/List01.java
+++ studentJava/src/exListas/List01.java
@@ -6,21 +6,29 @@ public class List01 {
     public static void main(String[] args) {
 
         //Lista do tipo String e está sendo inicializada ArrayList
+
         java.util.List<String> list = new ArrayList<>();
 
+        // Adicionando nomes da lista
         list.add("Maria");
         list.add("Alex");
         list.add("Bob");
         list.add("Anna");
+        //adicionando em uma posição especifiica
         list.add(2,"Marco");
 
+        //Mostrar o tamanho de posiçoes
         System.out.println(list.size());
 
         list.remove(1);
         for (String nome: list) {
             System.out.println(nome);
         }
-
+        System.out.println("---------------------------");
+        list.removeIf(x -> x.charAt(0) == 'M');
+        for (String nome: list) {
+            System.out.println(nome);
+        }
 
     }
 }

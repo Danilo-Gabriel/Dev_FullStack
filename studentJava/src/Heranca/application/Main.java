47d2271b05715d9ad8661e0673f0df18acd016ad
daniloGabriel <danilonascimento.dr@gmail.com>
2023-09-23 16:08:28 -0300

    UP

--- studentJava/src/Heranca/application/Main.java
+++ studentJava/src/Heranca/application/Main.java
@@ -7,22 +7,11 @@ import Heranca.entities.SavingsAccount;
 public class Main {
     public static void main(String[] args) {
 
-        Account acc = new Account(1001, "Alex", 0.0);
+        Account x = new Account(1020, "Alex",1000.0);
+        Account y = new SavingsAccount(2023, "Maria", 1100.0, 0.01);
 
-        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
-
-
-        // UPCASTING
-        Account acc1 = bacc;
-        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-        Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
-
-
-
-        //DOWNCASTING
-        BusinessAccount acc5 = (BusinessAccount)acc2;
-        acc5.loan(100.0);
-
+        x.withdraw(50.0);
+        y.withdraw(50.0);
 
     }
 }

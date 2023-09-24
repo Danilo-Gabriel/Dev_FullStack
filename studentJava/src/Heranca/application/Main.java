8ef4e752ef98d9b9037c71961063716f159720bb
daniloGabriel <danilonascimento.dr@gmail.com>
2023-09-24 09:19:06 -0300

    UP

--- studentJava/src/Heranca/application/Main.java
+++ studentJava/src/Heranca/application/Main.java
@@ -1,17 +1,62 @@
 package Heranca.application;
 
-import Heranca.entities.Account;
-import Heranca.entities.BusinessAccount;
-import Heranca.entities.SavingsAccount;
+import Heranca.Entities.Employee;
+import Heranca.Entities.OutsourcedEmployee;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
-        Account x = new Account(1020, "Alex",1000.0);
-        Account y = new SavingsAccount(2023, "Maria", 1100.0, 0.01);
+        public class Main {
 
-        x.withdraw(50.0);
-        y.withdraw(50.0);
+            public static void main(String[] args) {
+                Locale.setDefault(Locale.US);
+                Scanner sc =  new Scanner(System.in);
+                List<Employee> employeeList = new ArrayList<>();
+
+                int N;
+
+                System.out.println("Enter the number of employees: ");
+                N = sc.nextInt();
+
+                sc.nextLine();
+
+                for (int i = 1; i <= N; i++) {
+
+                    System.out.println("Emplooye # "+i+" Data");
+                    System.out.println("Outsourced (Y/N)");
+                    char R = sc.next().charAt(0);
+                    System.out.println("Nome: ");
+                    sc.nextLine();
+                    String name = sc.nextLine();
+                    System.out.println("Hours: ");
+                    Double hours = sc.nextDouble();
+                    System.out.println("Value per Hour: ");
+                    Double valuePerHora = sc.nextDouble();
+                    if(R == 'Y') {
+                        System.out.println("Addiotinal charge: ");
+                        Double addiotanalCharge = sc.nextDouble();
+                        Employee emp = new OutsourcedEmployee(name, hours, valuePerHora,addiotanalCharge);
+                        //employeeList.add(new OutsourcedEmployee(name, hours, valuePerHora,addiotanalCharge));
+                        employeeList.add(emp);
+
+                    }
+
+                    else {
+                        Employee emp = new Employee(name, hours, valuePerHora);
+                        employeeList.add(emp);
+                    }
+
+                }
+
+                for (Employee x: employeeList) {
+                    System.out.println(x.toString());
+                }
+                sc.close();
+
+
+
 
     }
 }

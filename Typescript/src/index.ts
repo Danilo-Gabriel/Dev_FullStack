73dd68df8467cfa40724d3a67b973b87de2d1cb3
daniloGabriel <danilonascimento.dr@gmail.com>
2024-02-05 11:02:13 -0300

    REF:Update

---
+++ Typescript/src/index.ts
@@ -0,0 +1,13 @@
+
+function sum(a : number, b : number) : number{
+    return a + b;
+}
+
+
+function greetinf(name : string , gret?: string){
+
+    console.log(`olá, ${gret}, ${name}`)
+
+}
+
+greetinf("Danilo")
--- files-storage-api/src/main/java/br/com/danilo/filesstorageapi/FileStorageController.java
+++ files-storage-api/src/main/java/br/com/danilo/filesstorageapi/FileStorageController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
